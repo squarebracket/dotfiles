5cd678ae34620548db4dcfd1a459b9498606b43f
Chuck Wilson <chuck.wilson@gmail.com>
2015-10-07 18:59:40 -0400

    Adding non-automatic bash stuff

---
+++ bash-nonauto/eca-aliases.sh
@@ -0,0 +1,6 @@
+alias transistor='ssh -XC -p 24 sysadmin@132.205.15.53'
+alias odysseus='ssh root@132.205.15.53'
+alias homer="ssh root@132.205.15.56"
+alias web="ssh -X -p 27 sysadmin@132.205.15.53"
+alias nitrogen="ssh -X -p 26 administrator@132.205.15.53"
+alias ad="ssh -X -p 28 sysadmin@132.205.15.53"
