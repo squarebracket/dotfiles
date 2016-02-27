2a2ecc94d2912862c21dc6a1d60632e1d8c76849
Chuck Wilson <charles.wilson@vantrix.com>
2016-02-27 02:18:53 -0500

    enabling allow-rename on everything

--- setup-environment.sh
+++ setup-environment.sh
@@ -6,26 +6,38 @@ if [ ! -d ".dotfiles-$REMOTE_USER" ]; then
     echo "Getting the dotfiles"
     git clone https://squarebracket@bitbucket.org/squarebracket/dotfiles.git .dotfiles-$REMOTE_USER
     echo "Making links"
-    ln -s ~/.dotfiles-$REMOTE_USER/vim/vimrc ~/.vimrc-$REMOTE_USER
-    ln -s ~/.dotfiles-$REMOTE_USER/tmux/tmux.conf ~/.tmux.conf-$REMOTE_USER
-    ln -s ~/.dotfiles-$REMOTE_USER/screen/.screenrc ~/.screenrc-$REMOTE_USER
-    ln -s ~/.dotfiles-$REMOTE_USER/dircolors ~/.dircolors-$REMOTE_USER
-    ln -s ~/.dotfiles-$REMOTE_USER/bash/bashrc ~/.bashrc-$REMOTE_USER
+    ln -fs ~/.dotfiles-$REMOTE_USER/vim ~/.vim
+    ln -fs ~/.vim/vimrc ~/.vimrc-$REMOTE_USER
+    ln -fs ~/.dotfiles-$REMOTE_USER/tmux/tmux.conf ~/.tmux.conf-$REMOTE_USER
+    ln -fs ~/.dotfiles-$REMOTE_USER/screen/.screenrc ~/.screenrc-$REMOTE_USER
+    ln -fs ~/.dotfiles-$REMOTE_USER/dircolors ~/.dircolors-$REMOTE_USER
+    ln -fs ~/.dotfiles-$REMOTE_USER/bash/bashrc ~/.bashrc-$REMOTE_USER
+    ln -fs ~/.dotfiles-$REMOTE_USER/oh-my-zsh ~/.oh-my-zsh
+    ln -fs ~/.dotfiles-$REMOTE_USER/zsh/zshrc ~/.zshrc-$REMOTE_USER
+    ln -fs ~/.dotfiles-$REMOTE_USER/weechat ~/.weechat
     echo "Installing public key"
-    cat ~/.dotfiles-$REMOTE_USER/public-key >> ~/.ssh/known_hosts
+    for file in `ls ~/.dotfiles-$REMOTE_USER/keys/*`; do
+        cat $file >> ~/.ssh/authorized_keys
+    done
     # if you need to install software or do anything else before launching
     # your fancy new environment, add it to ./required.sh
     if [ -f .dotfiles-$REMOTE_USER/required.sh ]; then
         echo "Performing required pre-environment actions"
         bash .dotfiles-$REMOTE_USER/required.sh
     fi
+    if [ -f .dotfiles-$REMOTE_USER/requirements.txt ]; then
+        echo "PIP"
+        pip install -r .dotfiles-$REMOTE_USER/requirements.txt
+    fi
     # Test to see if we have ssh keys set up for remote copying back to host
     ssh -q -o PasswordAuthentication=no -p 5555 $REMOTE_USER@localhost -t 'echo "success!"'
-    if [ $? != 0 ]; then
+    EXIT_CODE=$?
+    if [ $EXIT_CODE != 0 ]; then
         # If we don't, exit with status 13
         exit 13
     fi
 fi
+printk '\033]2;%s\033\\' "$LAUNCH_SHELL"
 if [ "$LAUNCH_SHELL" = "screen" ]; then
     screen -c ~/.screenrc-$REMOTE_USER
 elif [ "$LAUNCH_SHELL" = "tmux" ]; then
