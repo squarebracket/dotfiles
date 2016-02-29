eb74329e598c31ed30bd63eb7cf7f54e58d24d87
Chuck Wilson <charles.wilson@vantrix.com>
2016-02-29 16:28:51 -0500

    A bunch of changes

--- setup-environment.sh
+++ setup-environment.sh
@@ -5,13 +5,18 @@ export LOOPBACK_PORT=$3
 cd ~
 export DOTFILES="~/.dotfiles-$REMOTE_USER"
 export ENVIRONMENT=$ENVIRONMENT
-git --version > /dev/null
+
 # detect installer
 yum --version > /dev/null && [ $? = 0 ] && export INSTALLER=yum
 apt-get --version > /dev/null && [ $? = 0 ] && export INSTALLER=apt-get
+
+# Detect if we have git
+git --version > /dev/null
 if [ $? != 0 ]; then
     $INSTALLER install -y git
 fi
+
+# Do the dotfiles magic if the folder doesn't exist
 if [ ! -d ".dotfiles-$REMOTE_USER" ]; then
     echo "Getting the dotfiles"
     git clone https://squarebracket@bitbucket.org/squarebracket/dotfiles.git .dotfiles-$REMOTE_USER
@@ -59,11 +64,23 @@ if [ ! -d ".dotfiles-$REMOTE_USER" ]; then
 else
     cd .dotfiles-$REMOTE_USER
     git pull
-    git submodule update --init --force
+    git submodule update --init
     cd ~
 fi
+
 printf '\033]2;%s\033\\' "$LAUNCH_SHELL"
-#echo "$LAUNCH_SHELL"
+
+case "$SHELL" in
+/bin/bash)
+    export CUSTOM_SHELL="$SHELL --rcfile ~/.dotfiles-$REMOTE_USER/bash/bashrc"
+    echo "shell is: $SHELL"
+    ;;
+zsh)
+    export CUSTOM_SHELL="$SHELL"
+    export ZDOTDIR="~/.dotfiles-$REMOTE_USER/zsh/"
+    ;;
+esac
+
 if [ "$LAUNCH_SHELL" = "screen" ]; then
     screen -c ~/.screenrc-$REMOTE_USER
 elif [ "$LAUNCH_SHELL" = "tmux" ]; then
