04d3584a90e90ff4f2916fbcaae248a597943537
Chuck Wilson <charles.wilson@vantrix.com>
2016-03-03 13:51:05 -0500

    Editing for active/failed services

--- powerline/vantrix.py
+++ powerline/vantrix.py
@@ -3,6 +3,9 @@ import os
 
 from powerline.segments import Segment
 
+DOTFILES = os.environ['DOTFILES']
+VAN_HUD_FILE = os.path.join('~', DOTFILES, 'shared-shell-scripts', 'vantrix', 'van-hud')
+
 class VantrixServices(Segment):
     
     def __call__(self, pl):
@@ -13,8 +16,13 @@ def puppet_running(pl):
     if os.path.isfile('/var/lib/puppet/state/agent_catalog_run.lock'):
         return "Applying puppet"
 
-def van_services(pl):
-    p = subprocess.Popen(['bash', '/root/.van_services.sh'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+def van_active_services(pl):
+    p = subprocess.Popen(['bash', VAN_HUD_FILE, '--failed-services'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = p.communicate()
-
     return out
+
+def van_failed_services(pl):
+    p = subprocess.Popen(['bash', VAN_HUD_FILE, '--failed-services'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    return out
+
