91c68db660fd80eda70bf1f3128bf7b5e28d4cce
Chuck Wilson <charles.wilson@vantrix.com>
2016-03-02 19:14:04 -0500

    Code cleanup and adding some environment vars
    - Moving `$LOOPBACK_PORT` from a parameter to an environment variable.
      This makes it an easy check for if we should establish the reverse
      tunnel.
    - Added $POWERLINE_FONT environment var
    - Parameterized repo to clone as `$REPO` environment variable
    - Documentation and misc cleanup

--- setup-environment.sh
+++ setup-environment.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
+# export arguments to script
 export REMOTE_USER=$1
 export LAUNCH_SHELL=$2
-export LOOPBACK_PORT=$3
-REPO=
+# set locally-used variables which get overridden by environment
+REPO=${REPO:=https://github.com/squarebracket/dotfiles.git}
+# export important environment variables
+export LOOPBACK_PORT=$LOOPBACK_PORT
+export POWERLINE_FONT=$POWERLINE_FONT
+export ENVIRONMENT=$ENVIRONMENT
+# make sure we're in ~
 cd ~
+# set and export $DOTFILES
 if [ "$REMOTE_USER" = "LOCAL" ]; then
     export DOTFILES="dotfiles"
     TERMINATOR=''
@@ -11,33 +18,33 @@ else
     export DOTFILES=".dotfiles-$REMOTE_USER"
     TERMINATOR="-$REMOTE_USER"
 fi
-export ENVIRONMENT=$ENVIRONMENT
 
 # detect installer
 yum --version > /dev/null && [ $? = 0 ] && export INSTALLER=yum
 apt-get --version > /dev/null && [ $? = 0 ] && export INSTALLER=apt-get
 # Are we root?
 if [ "$UID" != "0" ]; then
+    # if not, set $SUDO
     export SUDO="sudo"
 fi
 
+# Tiny little function to install stuff if need it
+# It's very error-prone considering some binaries (like pip) have
+# a package name that differs from the binary name
 _install_if_needed() {
     which $1 > /dev/null
     if [ $? = 1 ]; then
         $SUDO $INSTALLER install -y $1
     fi
 }
-## Detect if we have git
-#git --version > /dev/null
-#if [ $? != 0 ]; then
-    #$SUDO $INSTALLER install -y git
-#fi
+
+# Install git if it's not present
 _install_if_needed git
 
 # Do the dotfiles magic if the folder doesn't exist
 if [ ! -d "$DOTFILES" ]; then
     echo "Getting the dotfiles"
-    git clone https://github.com/squarebracket/dotfiles.git $DOTFILES
+    git clone $REPO $DOTFILES
     
     echo "Pulling any submodules"
     cd $DOTFILES
@@ -57,7 +64,7 @@ if [ ! -d "$DOTFILES" ]; then
     # Vim is fucking dumb and can't handle using alternate .vimrc files,
     # so we have to literally append some bullshit to ~/.vimrc just to make
     # it work
-    echo "Verifying vimrc hack"
+    echo "Managing vimrc hack"
     if [ ! -f ~/.vimrc ]; then
        echo 'source ~/$DOTFILES/vim/vimrc' >> ~/.vimrc
     elif  [ -f ~/.vimrc ]; then
@@ -75,8 +82,10 @@ if [ ! -d "$DOTFILES" ]; then
         ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N ''
     fi
 
-    # if you need to install software or do anything else before launching
-    # your fancy new environment, add it to ./required.sh
+    # If you need to install software or do anything else before launching
+    # your fancy new environment, add it to $DOTFILES/required.sh
+    # Remember that $INSTALLER and $SUDO are exported above -- you should
+    # use those variables for installing software
     if [ -f $DOTFILES/required.sh ]; then
         echo "Performing required pre-environment actions"
         bash $DOTFILES/required.sh
@@ -86,21 +95,25 @@ if [ ! -d "$DOTFILES" ]; then
     # requirements.txt file
     if [ -f $DOTFILES/requirements.txt ]; then
         echo "installing pip requirements..."
+        # Custom test/install for pip
         pip --version > /dev/null
         if [ $? != 0 ]; then $SUDO $INSTALLER install -y python-pip; fi
+        # Do the installing
         $SUDO pip install -r $DOTFILES/requirements.txt
     fi
 
-    if [ "$REMOTE_USER" != "LOCAL" ]; then
-        # Test to see if we have ssh keys set up for remote copying back to host
+    # If we're remote and we've got a $LOOPBACK_PORT...
+    if [ "$REMOTE_USER" != "LOCAL" -a -n "$LOOPBACK_PORT" ]; then
+        # ...test to see if we have ssh keys set up for remote copying back to host...
         ssh -q -o PasswordAuthentication=no -p 5555 $REMOTE_USER@localhost -t 'echo "success!"'
         EXIT_CODE=$?
         if [ $EXIT_CODE != 0 ]; then
-            # If we don't, exit with status 13
+            # ...and if we don't, exit with status 13
             exit 13
         fi
     fi
 
+# Update the repo(s) on every login
 else
     # Update the dotfiles and all submodules to the latest version
     cd $DOTFILES
@@ -109,14 +122,16 @@ else
     cd ~
 fi
 
+# Set the title of the pane for embedded screen/tmux
 if [ "$REMOTE_USER" != "LOCAL" ]; then
     printf '\033]2;%s\033\\' "$LAUNCH_SHELL"
 fi
 
+# Your local and remote shell may be different, but you probably still want
+# a customized environment. So here we set up a custom variable if needed.
 case "$SHELL" in
 /bin/bash)
     export CUSTOM_SHELL="$SHELL --rcfile ~/$DOTFILES/bash/bashrc"
-    echo "shell is: $SHELL"
     ;;
 /bin/zsh)
     export CUSTOM_SHELL="$SHELL"
@@ -124,11 +139,14 @@ case "$SHELL" in
     ;;
 esac
 
+echo "shell is: $SHELL"
 _install_if_needed $LAUNCH_SHELL
 
 if [ "$LAUNCH_SHELL" = "screen" ]; then
     screen -c ~/.screenrc$TERMINATOR $CUSTOM_SHELL
 elif [ "$LAUNCH_SHELL" = "tmux" ]; then
     tmux has-session -t $REMOTE_USER && tmux attach -t $REMOTE_USER || tmux -f ~/.tmux.conf$TERMINATOR new-session -s $REMOTE_USER "$CUSTOM_SHELL"
+else
+    $LAUNCH_SHELL
 fi
 
